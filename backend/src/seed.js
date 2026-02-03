a0b9e727835529b3fd5c0d2668fc33de80060787
SherlynPeqs <lr.sjpequit@mmdc.mcl.edu.ph>
2026-02-03 20:09:49 +0800

    Update seed.js
    Changed process.env.MONGO_URI to process.env.MONGODB_URI

--- backend/src/seed.js
+++ backend/src/seed.js
@@ -12,7 +12,7 @@ const data = [
 
 async function run() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     await Product.deleteMany({});
     await Product.insertMany(data);
     console.log("✅ Seed complete. Inserted:", data.length);
